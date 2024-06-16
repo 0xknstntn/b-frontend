ee0b460d4d498de3d7aa659e44d7ee903e7a4a5b
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-06-16 23:53:28 +0500

    feat: deposit page

--- src/components/Page/DepositMiner/DepositMiner.tsx
+++ src/components/Page/DepositMiner/DepositMiner.tsx
@@ -1,14 +1,126 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 import styled from "styled-components";
+import NFTASIC from '../../../assets/laptop.webp'
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Container = styled.div`
+    width: 85%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const NameContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-top: 50px;
+`
+
+const Logo = styled.img`
+    width: 25px;
+    height: 25px;
+    margin-left: 2px;
+`
+
+const Name = styled.a`
+    color: #fff;
+    font-size: 15px;
+    font-weight: 500;
+    margin-left: 10px;
+`
+
+const AmountContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+`
+
+const Input = styled.input`
+    max-width: 230px;
+    min-width: 1ch;
+    height: 50px;
+    font-size: 55px;
+    font-weight: 500;
+    color: #fff;
+    background: transparent;
+    padding: 0;
+    ::placeholder { 
+    color: #fff;
+}
+`
+
+const WithdrawNameToken = styled.a`
+    color: #a5a5a5;
+    font-size: 38px;
+    font-weight: 500;
+    margin-top: 10px;
+    margin-left: 7px;
+`
+
+const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: -5px;
+`
+
+const AmountOnBalance = styled.a`
+    color: #a5a5a5;
+    font-size: 15px;
+    font-weight: 500;
+    margin-top: 10px;
+    margin-left: 2px;
+`
+
+const ButtonContainer = styled.div`
+    width: 100%;
+    height: 45px;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    bottom: 20px;
+`
+
+const NonActiveConfirm = styled.button`
     width: 90%;
+    height: 45px;
+    border-radius: 10px;
+    background-color: #2f2f2f;
+    color: #555;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+`
+
+const ActiveConfirm = styled.button`
+    width: 90%;
+    height: 45px;
+    border-radius: 10px;
+    background-color: #3e87f6;
+    color: #fff;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+`
+
+const Links = styled(Link)`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
 `
 
 
 export const DepositMiner = () => {
 
+    const [amount, setAmount] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,9 +128,31 @@ export const DepositMiner = () => {
         window.Telegram.WebApp.BackButton.onClick(() => navigate(-1))
 	}, [])
 
-    return(
-        <Container>
-            <h1 style={{color: "#fff"}}>Deposit Miner</h1>
-        </Container>
+    return (
+        <>
+            <Container>
+                <div style={{ width: "100%" }}>
+                    <NameContainer>
+                        <Logo src={NFTASIC} />
+                        <Name>Deposit NFT Miner</Name>
+                    </NameContainer>
+                    <AmountContainer>
+                        <InputContainer>
+                            <Input
+                                value={amount}
+                                style={{ maxWidth: `${amount.length}ch` }}
+                                onChange={(e) => setAmount(e.target.value)}
+                                inputMode='numeric'
+                                placeholder="0"></Input>
+                            <WithdrawNameToken>NFT</WithdrawNameToken>
+                        </InputContainer>
+                        <AmountOnBalance>0 NFT on balance</AmountOnBalance>
+                    </AmountContainer>
+                </div>
+            </Container>
+            <ButtonContainer>
+                {amount != "" ? <ActiveConfirm>CONTINUE</ActiveConfirm> : <NonActiveConfirm>CONTINUE</NonActiveConfirm>}
+            </ButtonContainer>
+        </>
     )
 }
