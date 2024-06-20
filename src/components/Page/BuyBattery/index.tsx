08fd8cb4ccaa259d3f5d5ee267150da3138710fa
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-06-20 20:04:18 +0500

    test:

--- src/components/Page/BuyBattery/index.tsx
+++ src/components/Page/BuyBattery/index.tsx
@@ -42,7 +42,7 @@ const Input = styled.input`
     min-width: 1ch;
     height: 60px;
     font-size: 55px;
-    font-weight: 500;
+    font-weight: 400;
     color: #fff;
     background: transparent;
     padding: 0;
@@ -53,7 +53,6 @@ const WithdrawNameToken = styled.a`
     font-size: 30px;
     font-weight: 500;
     margin-top: 10px;
-    margin-left: 5px;
 `
 
 const InputContainer = styled.div`
