3860ba64a19100876494bdc5798685de3ee9933d
knstntn <konstantin@quadrate.org>
2024-06-23 19:01:45 +0500

    fix

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,1 @@
-export const BytecoinProtocolAddress = 'kQDgle7nXgxjFu-q6jLMW96z-FwtlXEQvSVv56XcdzmeAZLo'
+export const BytecoinProtocolAddress = '0:e095eee75e0c6316efaaea32cc5bdeb3f85c2d957110bd256fe7a5dc77399e01'
